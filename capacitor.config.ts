8b378ff5b96a3cb7367d0f3826c293754cb35615
rolaaagp <143853171+rolaaagp@users.noreply.github.com>
2024-12-09 09:33:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pr_cp_imgs',
+  webDir: 'www'
+};
+
+export default config;
